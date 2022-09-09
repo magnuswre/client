2cd4366e643ba3684be0d884ca8c8145e17f74ad
magnuswretblad <mwdev81@gmail.com>
2022-09-09 13:33:25 +0200

    created src folder
    Somethingh

---
+++ src/register/register.js
@@ -0,0 +1,51 @@
+let user = document.getElementById("username")
+let registerUser = document.getElementById("register")
+let button1 = document.getElementById("btn2")  
+let button3 = document.getElementById("btn1")
+
+  // function changeImage(){} det här var funktionen   
+  
+   // TRE VARIANTER add a function ta a button. 
+//button3.onclick = changeImage; // connecta med id
+//button3.addEventListener("click", changeImage)  // eller gör såhär med eventlistener sprint 13: 0500
+button3.addEventListener("click", ()=>{ // 07:00 i sprint 13 () Man behöver inte ge ett namn i parentesen om det bara används en gång
+// om functionen/knappen ska användas EN gång är den sista bäst. Men ska den användas fler gånger ta en annan 
+  let image = document.querySelector("img") //access img from the DOM (kan vara id eller class)sparar i en varibl
+  image.setAttribute("src", allImages[indexNumber]) // 10:00 sprint 12 Här andrar vi attributet. setAttribute. // Kan skapa ett som inte finns eller ändra ett som finns 
+   
+  if(indexNumber === allImages.length-1){ // ta length minus ett, då får man samma samma positon som indexnummer. sen kan man increment/öka med ett. (++;) 
+    indexNumber = 0;
+  }else{
+    indexNumber++;   //här ökas indexnumber med ett varje gång knappen trycks 
+} 
+
+});
+
+
+
+function getUser(){
+   user.innerHTML = registerUser.value
+   registerUser.value = " ";  // här "nollställs" placeholdern efter man skrivit något i den. 31:00 i sprint 11 
+   
+}
+
+button1.onclick = getUser;
+
+let allImages=[
+  "images/dog.jpg", 
+  "images/cat.jpg", 
+  "images/snake.jpg", 
+  "images/rodent.jpg" 
+] // byt ut dessa till kattbilder
+
+let indexNumber = 1; // det här blir nästa indexnummer från allImages när knappen trycks
+
+
+
+function toggleDarkMode(){
+  let container = document.querySelector(".dark-mode")
+
+  container.classList.toggle("darkMode"); //TOGGLE 23:00 sprint 12
+  
+
+}
---
+++ src/script.js
@@ -0,0 +1,22 @@
+const http = require("http");
+const host = 'localhost';
+const port = 8000;
+const express = require("express");
+const res = require("express/lib/response"); 
+const app = express();
+const bodyParser = require("body-parser") //importar funktioner, hjälper backend vilken typ av jsondata som kommer från frontend 34:10 i Backend 3 MIMO LIVE   
+app.use(bodyParser.json()) //make it understand what json "do"
+
+
+app.get("/", (req, res)=>{ // endpoint med bara forward slash / är hemsidan. 
+    res.status(200).json({message:"Welcome to the server"})
+})
+/*
+fetch(`http://localhost:5000/users?q=magnus1`)
+.then((res)=>console.log(res))  
+*/
+
+app.listen(port, ()=>{
+    console.log(`Server is running at port ${port}`)
+    
+})

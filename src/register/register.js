914b33be0138ed0351181a523292941242c7cde4
magnuswretblad <mwdev81@gmail.com>
2022-09-16 18:01:48 +0200

    worked with create/register new user

--- src/register/register.js
+++ src/register/register.js
@@ -3,9 +3,25 @@
 
 // -----------------REGISTRATE NEW USER--------------------// 
 
+/*
+const data = { username: 'example' };
 
+fetch('https://petrescuenow.herokuapp.com/users/register', {
+  method: 'POST', // or 'PUT'
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(data),
+})
+  .then((response) => response.json())
+  .then((data) => {
+    console.log('Success:', data);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
 
-
+*/
 
 
 

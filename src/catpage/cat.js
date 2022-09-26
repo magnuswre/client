03e324cf740736ea9586a6540b830c0650209909
magnuswretblad <mwdev81@gmail.com>
2022-09-26 16:52:16 +0200

    depoyy

--- src/catpage/cat.js
+++ src/catpage/cat.js
@@ -1,11 +1,92 @@
+let placeholder = document.getElementById("insert-text")
+let tillbakaText = document.getElementById("tillbaka-text")
+let btn = document.getElementById("submit")
+let svarsContainer = document.getElementById("svars-container")
 
-//---- GET USER INPUT AND DISPLAY ON PAGE----// 
+btn.onclick = visaText 
+// function visaText(){
+//     console.log(placeholder)
 
-const formContainer = document.getElementById("form-container"); 
-const userInput = document.getElementById("input-area")
-let userName = document.getElementById("user-name")
+// }
 
-userInput.value
+
+
+
+
+
+
+// function visaText(){
+
+//     // tillbakaText.innerHTML = placeholder.value
+
+// // let updatetext = placeholder.value 
+// let nyText = document.createElement('p')
+//  nyText.innerHTML = placeholder.value
+
+// nyText.setAttribute("id", "search")
+// svarsContainer.appenChild(nyText)
+
+// placeholder = " ";
+// }
+
+
+
+
+// document.getElementById("demo")
+
+// addEventListener("click", minFunktion)
+
+// function minFunktion(){
+//     document.getElementById("demo").innerHTML = "Hej Henkin";
+// console.log("works")
+
+// }
+
+// let myinput = document.getElementById("input")
+
+// function hej(){
+// var x = document.getElementById("input").value
+// document.getElementById("demo").innerHTML = "hej" + x;
+// }
+
+
+// let uploadBtn = document.getElementById("upload-btn")
+// let imageUpload = document.getElementById("image-upload")
+
+// let demo = document.getElementById("demo")
+
+// function handleUpload(){
+//     demo.innerHtml = 
+// }
+
+// uploadBtn.addEventlistener("click", handleUpload)
+
+
+
+
+// {/* <button id="myBtn">Try it</button>
+
+// <p id="demo">
+
+// <script>
+// const element = document.getElementById("myBtn");
+// element.addEventListener("click", myFunction);
+
+// function myFunction() {
+//   document.getElementById("demo").innerHTML = "Hello World";
+// }
+// </script> */}
+
+
+
+
+// // //---- GET USER INPUT AND DISPLAY ON PAGE----// 
+
+// // const formContainer = document.getElementById("form-container"); 
+// // const userInput = document.getElementById("input-area")
+// // let userName = document.getElementById("user-name")
+
+// // userInput.value
 
 
 
